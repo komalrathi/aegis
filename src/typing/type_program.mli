fe150a5f0407262f5700c309cd06b3acea5c0ed2
Komal Rathi <komalrathi@dhcp-10-249-99-150.eduroam.wireless.private.cam.ac.uk>
2024-11-27 22:27:23 +0000

    move type_expr into a separate file

--- src/typing/type_program.mli
+++ src/typing/type_program.mli
@@ -1,10 +1,4 @@
 open Core
-open Compiler_types.Ast_types
-open Compiler_types.Language_types
 open Parser_frontend
-open Type_environment
-
-val type_expr :
-  Parsed_ast.expr -> type_environment -> (type_expr * Typed_ast.expr) Or_error.t
 
 val type_program : Parsed_ast.program -> Typed_ast.program Or_error.t
