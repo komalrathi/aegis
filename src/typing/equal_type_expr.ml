62b41eef58304cb8d7770c37e46f4d6cd0a901a2
Komal Rathi <komalrathi@dhcp-10-249-96-84.eduroam.wireless.private.cam.ac.uk>
2024-12-15 10:56:01 +0000

    dune format

--- src/typing/equal_type_expr.ml
+++ src/typing/equal_type_expr.ml
@@ -1,4 +1,4 @@
-open Core 
+open Core
 open Compiler_types.Language_types
 
 let rec equal_core_type a b =
@@ -6,11 +6,13 @@ let rec equal_core_type a b =
   | TEInt, TEInt -> true
   | TEBool, TEBool -> true
   | TFunction (args_a, ret_a), TFunction (args_b, ret_b) ->
-      List.length args_a = List.length args_b &&
-      (match List.for_all2 ~f:(fun x y -> equal_core_type x y) args_a args_b with
-      | Ok result -> result
-      | Unequal_lengths -> false) &&
-      equal_core_type ret_a ret_b
+      List.length args_a = List.length args_b
+      && ( match
+             List.for_all2 ~f:(fun x y -> equal_core_type x y) args_a args_b
+           with
+         | Ok result -> result
+         | Unequal_lengths -> false )
+      && equal_core_type ret_a ret_b
   | _, _ -> false
 
 let equal_security_level_type a b =
@@ -20,6 +22,6 @@ let equal_security_level_type a b =
   | _, _ -> false
 
 let equal_type_expr a b =
-  let (core_a, sec_a) = a in
-  let (core_b, sec_b) = b in
+  let core_a, sec_a = a in
+  let core_b, sec_b = b in
   equal_core_type core_a core_b && equal_security_level_type sec_a sec_b
