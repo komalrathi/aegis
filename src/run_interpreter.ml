d90328e2fca82088be85c1e344a69164a727253b
Komal Rathi <komalrathi@dhcp-10-249-96-84.eduroam.wireless.private.cam.ac.uk>
2024-12-15 11:00:01 +0000

    separate result printing from function in run_interpreter

--- src/run_interpreter.ml
+++ src/run_interpreter.ml
@@ -1,5 +1,6 @@
 open Parser_frontend.Parse_program
 open Core
+open Typing
 open Typing.Type_program
 open Interpreter.Interpret_program
 open Compiler_types.Language_types
@@ -8,9 +9,19 @@ let string_of_val = function
   | VInt i -> string_of_int i
   | VBool b -> string_of_bool b
 
-let run_interpreter lexbuf =
-  let ( >>= ) = Result.( >>= ) in
-  parse_program lexbuf >>= type_program >>= interpret_program
-  |> function
+let print_result = function
   | Ok v -> printf "Success: %s" (string_of_val v)
   | Error e -> eprintf "%s" (Error.to_string_hum e)
+
+(* let run_interpreter lexbuf = let ( >>= ) = Result.( >>= ) in parse_program
+   lexbuf >>= type_program >>= interpret_program |> function | Ok v -> printf
+   "Success: %s" (string_of_val v) | Error e -> eprintf "%s"
+   (Error.to_string_hum e) *)
+
+let run_interpreter lexbuf =
+  let ( >>= ) = Result.( >>= ) in
+  match parse_program lexbuf >>= type_program with
+  | Ok (typed_fn_defns, typed_expr) ->
+      let typed_program = Typed_ast.Prog (typed_fn_defns, typed_expr) in
+      interpret_program typed_program |> print_result
+  | Error e -> print_result (Error e)
