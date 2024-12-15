c5f1bc050fee0431eb1c3cbf06ab89864e07321d
Komal Rathi <komalrathi@dhcp-10-249-96-84.eduroam.wireless.private.cam.ac.uk>
2024-12-15 10:53:40 +0000

    separate interpret_expr and interpret_program into separate files + update interpret_program with updated arguments

--- src/interpreter/interpret_program.ml
+++ src/interpreter/interpret_program.ml
@@ -1,131 +1,14 @@
 open Core
 open Typing.Typed_ast
-open Compiler_types.Ast_types
-open Compiler_types.Language_types
-open Value_environment
+open Interpret_expr
 
-let apply_int_bin_op bin_op i1 i2 =
-  match bin_op with
-  | BinOpPlus -> Ok (VInt (i1 + i2))
-  | BinOpMinus -> Ok (VInt (i1 - i2))
-  | BinOpMultiply -> Ok (VInt (i1 * i2))
-  | BinOpDivide ->
-      if phys_equal i2 0 then Error (Error.of_string "Division by zero")
-      else Ok (VInt (i1 / i2))
-
-let apply_comp_op comp_op i1 i2 =
-  match comp_op with
-  | CompOpLessThan -> Ok (VBool (i1 < i2))
-  | CompOpGreaterThan -> Ok (VBool (i1 > i2))
-  | CompOpLessThanEqual -> Ok (VBool (i1 <= i2))
-  | CompOpGreaterThanEqual -> Ok (VBool (i1 >= i2))
-
-let interpret_bin_op bin_op i1 i2 =
-  match (i1, i2) with
-  | VInt i1, VInt i2 -> apply_int_bin_op bin_op i1 i2
-  | VInt _, VBool _ | VBool _, VInt _ | VBool _, VBool _ ->
-      Error
-        (Error.of_string
-           "Type error: cannot apply binary operation to boolean values" )
-
-let interpret_comp_op comp_op i1 i2 =
-  match (i1, i2) with
-  | VInt i1, VInt i2 -> apply_comp_op comp_op i1 i2
-  | VInt _, VBool _ | VBool _, VInt _ | VBool _, VBool _ ->
-      Error
-        (Error.of_string
-           "Type error: cannot apply comparison operation to boolean values" )
-
-let rec interpret_expr expr value_environment =
+(* let interpret_program (Prog (typed_fn_defns, typed_expr)) = (* let
+   initial_environment = [] in interpret_expr typed_expr initial_environment
+   *) let ( >>= ) = Result.( >>= ) in interpret_fn_defns typed_fn_defns []
+   >>= fun value_environment -> interpret_expr typed_expr
+   value_environment *)
+let interpret_program (Prog (typed_fn_defns, typed_expr)) =
   let ( >>= ) = Result.( >>= ) in
-  match expr with
-  | Integer (_, i, _) -> Ok (VInt i)
-  | Boolean (_, b, _) -> Ok (VBool b)
-  | BinOp (_, _, bin_op, e1, e2) ->
-      interpret_expr e1 value_environment
-      >>= fun val1 ->
-      interpret_expr e2 value_environment
-      >>= fun val2 -> interpret_bin_op bin_op val1 val2
-  | CompOp (_, _, comp_op, e1, e2) ->
-      interpret_expr e1 value_environment
-      >>= fun val1 ->
-      interpret_expr e2 value_environment
-      >>= fun val2 -> interpret_comp_op comp_op val1 val2
-  | BoolOp (_, _, bool_comp_op, e1, e2) -> (
-      interpret_expr e1 value_environment
-      >>= fun val1 ->
-      interpret_expr e2 value_environment
-      >>= fun val2 ->
-      match (val1, val2) with
-      | VBool b1, VBool b2 -> (
-        match bool_comp_op with
-        | BoolOpAnd -> Ok (VBool (b1 && b2))
-        | BoolOpOr -> Ok (VBool (b1 || b2)) )
-      | VInt _, VBool _ | VBool _, VInt _ | VInt _, VInt _ ->
-          Error
-            (Error.of_string
-               "Type error: cannot apply boolean operation to integer values" )
-      )
-  (* need to store the value of the identifier in the environment *)
-  | Identifier (_, var_type, identifier) -> (
-      lookup_var_value value_environment identifier
-      |> function
-      | None -> Error (Error.of_string "Variable does not have a value")
-      | Some var_value -> (
-        match (var_type, var_value) with
-        | (TEInt, _), VInt i -> Ok (VInt i)
-        | (TEBool, _), VBool b -> Ok (VBool b)
-        | _ ->
-            Error
-              (Error.of_string
-                 "Type error: variable type does not match value type" ) ) )
-  | Let (_, var_name, _, e1, e2, _) ->
-      interpret_expr e1 value_environment
-      >>= fun val1 ->
-      let new_env = (var_name, val1) :: value_environment in
-      interpret_expr e2 new_env
-  | Assign (_, var_type, var_name, e1) -> (
-      interpret_expr e1 value_environment
-      >>= fun val1 ->
-      match (var_type, val1) with
-      | (TEInt, _), VInt _ | (TEBool, _), VBool _ ->
-          let _ = (var_name, val1) :: value_environment in
-          Ok val1
-      | _ ->
-          Error
-            (Error.of_string
-               "Type error: variable type does not match value type" ) )
-  | If (_, e1, e2, e3, _) -> (
-      interpret_expr e1 value_environment
-      >>= fun val1 ->
-      match val1 with
-      | VBool true -> interpret_expr e2 value_environment
-      | VBool false -> interpret_expr e3 value_environment
-      | VInt _ ->
-          Error
-            (Error.of_string
-               "Type error: Cannot have int type in the if condition" ) )
-  | Classify (_, e1, e1_type_expr) -> (
-      interpret_expr e1 value_environment
-      >>= fun val1 ->
-      match (val1, e1_type_expr) with
-      | VInt i, (TEInt, TSHigh) -> Ok (VInt i)
-      | VBool b, (TEBool, TSHigh) -> Ok (VBool b)
-      | _ ->
-          Error
-            (Error.of_string
-               "Type error: cannot classify value with different type" ) )
-  | Declassify (_, e1, e1_type_expr) -> (
-      interpret_expr e1 value_environment
-      >>= fun val1 ->
-      match (val1, e1_type_expr) with
-      | VInt i, (TEInt, TSLow) -> Ok (VInt i)
-      | VBool b, (TEBool, TSLow) -> Ok (VBool b)
-      | _ ->
-          Error
-            (Error.of_string
-               "Type error: cannot classify value with different type" ) )
-
-let interpret_program (Prog expr) =
-  let initial_environment = [] in
-  interpret_expr expr initial_environment
+  interpret_fn_defns typed_fn_defns []
+  >>= fun function_environment ->
+  interpret_expr typed_expr [] function_environment
