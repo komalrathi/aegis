6430d218ceb5c0ec9cd2f18f4485121dd0529e30
Komal Rathi <komalrathi@Komals-MacBook-Pro.local>
2024-12-25 10:32:09 +0000

    add test for parsing good binary operators

--- test/expect/parsing/good_bin_op.ml
+++ test/expect/parsing/good_bin_op.ml
@@ -2,24 +2,27 @@ open Core
 open Print_parsed_ast
 
 let%expect_test "Binary Operation" =
-  print_parsed_ast (Lexing.from_string "5 * 6 / 2 + 7 - 1");
-  [%expect {|
+  print_parsed_ast (Lexing.from_string "5 * 6 / 2 + 7 - 1") ;
+  [%expect
+    {|
     Program([
 
     ], BinOp(Minus, BinOp(Plus, BinOp(Divide, BinOp(Multiply, Integer(5), Integer(6)), Integer(2)), Integer(7)), Integer(1)))
     |}]
 
 let%expect_test "Binary Operation with Parentheses" =
-  print_parsed_ast (Lexing.from_string "(5 * 6) / (2 + 7) - 1");
-  [%expect {|
+  print_parsed_ast (Lexing.from_string "(5 * 6) / (2 + 7) - 1") ;
+  [%expect
+    {|
     Program([
 
     ], BinOp(Minus, BinOp(Divide, BinOp(Multiply, Integer(5), Integer(6)), BinOp(Plus, Integer(2), Integer(7))), Integer(1)))
     |}]
 
 let%expect_test "Binary Operation with Parentheses and Precedence" =
-  print_parsed_ast (Lexing.from_string "5 * (6 / 2 + 7) - 1");
-  [%expect {|
+  print_parsed_ast (Lexing.from_string "5 * (6 / 2 + 7) - 1") ;
+  [%expect
+    {|
     Program([
 
     ], BinOp(Minus, BinOp(Multiply, Integer(5), BinOp(Plus, BinOp(Divide, Integer(6), Integer(2)), Integer(7))), Integer(1)))
