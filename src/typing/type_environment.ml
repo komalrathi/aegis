e1c275263a1c7a46fd8d88327b465ba4567a312c
Komal Rathi <komalrathi@dhcp-10-249-96-129.eduroam.wireless.private.cam.ac.uk>
2024-12-31 17:26:56 +0000

    fix bug - should have been performing struct equality not phys equality in lookup_var_type

--- src/typing/type_environment.ml
+++ src/typing/type_environment.ml
@@ -1,4 +1,4 @@
-open Core
+(* open Core *)
 open Compiler_types.Language_types
 open Compiler_types.Ast_types
 
@@ -6,20 +6,20 @@ type type_binding = identifier * type_expr
 
 type type_environment = type_binding list
 
-let rec lookup_var_type type_environment var =
+let rec lookup_var_type type_environment var_name =
   match type_environment with
   | [] -> None
-  | (list_var, list_var_type) :: t ->
-      if phys_equal var list_var then Some list_var_type
-      else lookup_var_type t var
+  | (list_var_name, list_var_type) :: t ->
+      if var_name = list_var_name then Some list_var_type
+      else lookup_var_type t var_name
 
 let get_function_types type_environment fn_name =
-  let ( >>= ) = Result.( >>= ) in
+  let ( >>= ) = Core.Result.( >>= ) in
   let rec get_function_types_helper type_environment fn_name =
     match type_environment with
-    | [] -> Error (Error.of_string "Function does not exist")
+    | [] -> Error (Core.Error.of_string "Function does not exist")
     | (var_name, var_type) :: t ->
-        if phys_equal fn_name var_name then
+        if fn_name = var_name then
           get_function_types_helper t fn_name
           >>= fun types -> Ok (var_type :: types)
         else get_function_types_helper t fn_name
