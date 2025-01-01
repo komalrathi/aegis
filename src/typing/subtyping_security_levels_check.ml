c1ae7293c05d1b955fb08aba8ec4ef4c654816e1
Komal Rathi <komalrathi@dhcp-10-249-126-37.eduroam.wireless.private.cam.ac.uk>
2025-01-01 21:26:45 +0000

    tidy comments in subtyping_security_levels_check.ml

--- src/typing/subtyping_security_levels_check.ml
+++ src/typing/subtyping_security_levels_check.ml
@@ -1,19 +1,19 @@
 open Compiler_types.Language_types
 
-(* Define the ordering relation for security levels *)
+(* ordering relation for security levels *)
 let less_than_or_equal l1 l2 =
   match (l1, l2) with
   | TSLow, _ -> true
   | TSHigh, TSHigh -> true
   | _ -> false
 
-(* Define the join operation for security levels *)
+(* join operation for pc and security levels *)
 let join l1 l2 =
   match (l1, l2) with
   | TSHigh, _ | _, TSHigh -> TSHigh
   | TSLow, TSLow -> TSLow
 
-(* Perform the subtyping check pc ⊔ l1 ⊑ l *)
+(* perform the subtyping check pc ⊔ l1 ⊑ l *)
 let subtyping_check pc l1 l =
   let joined_level = join pc l1 in
   less_than_or_equal joined_level l
