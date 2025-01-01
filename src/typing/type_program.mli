aab208dee72262202b35307f999978a7c94d5c0d
Komal Rathi <komalrathi@dhcp-10-249-126-37.eduroam.wireless.private.cam.ac.uk>
2025-01-01 19:17:49 +0000

    dune fmt

--- src/typing/type_program.mli
+++ src/typing/type_program.mli
@@ -2,5 +2,4 @@ open Core
 open Parser_frontend
 
 val type_program :
-     Parsed_ast.program
-   -> (Typed_ast.program, Error.t) Result.t
+  Parsed_ast.program -> (Typed_ast.program, Error.t) Result.t
