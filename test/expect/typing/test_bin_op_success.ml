dd5fd16640c26f316c70b9c10c27758db992d1d6
Komal Rathi <komalrathi@dhcp-10-249-96-129.eduroam.wireless.private.cam.ac.uk>
2025-01-03 20:54:22 +0000

    add expect test for typing of binary operations

--- test/expect/typing/test_bin_op_success.ml
+++ test/expect/typing/test_bin_op_success.ml
@@ -8,9 +8,33 @@ let%expect_test "Binary Operation" =
   with
   | Ok program -> print_typed_ast program
   | Error _ ->
-  [%expect.unreachable];
-  [%expect {|
+      [%expect.unreachable] ;
+      [%expect.unreachable] ;
+      [%expect.unreachable] ;
+      [%expect.unreachable] ;
+      [%expect.unreachable] ;
+      [%expect
+        {|
     Program([
 
     ], BinOp(Minus, (Int, Low), BinOp(Plus, (Int, Low), BinOp(Divide, (Int, Low), BinOp(Multiply, (Int, Low), Integer(5), Integer(6)), Integer(2)), Integer(7)), Integer(1)))
+    |}]
+
+let%expect_test "Binary Operation with Parentheses" =
+  match
+    Parser_frontend.Parse_program.parse_program
+      (Lexing.from_string "(5 * 6) / (2 + 7) - 1")
+  with
+  | Ok program -> print_typed_ast program
+  | Error _ ->
+      [%expect.unreachable] ;
+      [%expect.unreachable] ;
+      [%expect.unreachable] ;
+      [%expect.unreachable] ;
+      [%expect.unreachable] ;
+      [%expect
+        {|
+    Program([
+
+    ], BinOp(Minus, (Int, Low), BinOp(Divide, (Int, Low), BinOp(Multiply, (Int, Low), Integer(5), Integer(6)), BinOp(Plus, (Int, Low), Integer(2), Integer(7))), Integer(1)))
     |}]
