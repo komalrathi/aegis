b6edcb3214c097cbc628c5e3fcbc63558c10ee9d
Komal Rathi <komalrathi@dhcp-10-249-96-129.eduroam.wireless.private.cam.ac.uk>
2025-01-05 10:44:53 +0000

    add better error messages in type_function_defn

--- src/typing/type_function_defn.ml
+++ src/typing/type_function_defn.ml
@@ -10,10 +10,12 @@ let type_function_defn
     (Parsed_ast.FunctionDefn
        ( fn_name
        , args
-       , (return_core_type, return_security_level_type)
+       , (fn_return_core_type, fn_return_security_level_type)
        , expr_body ) ) type_env =
   let ( >>= ) = Result.( >>= ) in
-  let fn_return_type = (return_core_type, return_security_level_type) in
+  let fn_return_type =
+    (fn_return_core_type, fn_return_security_level_type)
+  in
   let arg_types_env =
     List.map
       ~f:(fun (TArg (arg_name, (arg_core_type, arg_security_level))) ->
@@ -23,18 +25,24 @@ let type_function_defn
   let arg_core_types =
     List.map ~f:(fun (TArg (_, (arg_core_type, _))) -> arg_core_type) args
   in
-  let fn_type = TFunction (arg_core_types, return_core_type) in
+  let fn_type = TFunction (arg_core_types, fn_return_core_type) in
   type_expr expr_body
-    ( (fn_name, (fn_type, return_security_level_type))
+    ( (fn_name, (fn_type, fn_return_security_level_type))
     :: (arg_types_env @ type_env) )
     TSLow
-  >>= fun (expr_body_type, typed_expr_body, _) ->
-  if equal_type_expr fn_return_type expr_body_type then
+  >>= fun ((expr_body_core_type, expr_body_sec_level), typed_expr_body, _) ->
+  if equal_type_expr fn_return_type (expr_body_core_type, expr_body_sec_level)
+  then
     Ok
       ( fn_name
-      , (fn_type, return_security_level_type)
+      , (fn_type, fn_return_security_level_type)
       , Typed_ast.FunctionDefn
           (fn_name, args, fn_return_type, typed_expr_body) )
+  else if equal_core_type fn_return_core_type expr_body_core_type then
+    Error
+      (Error.of_string
+         "The function security level return type does not match the \
+          function body security level type" )
   else
     Error
       (Error.of_string
