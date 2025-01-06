3d8ecae218a61e7444ad640a1a32f4aa89b95f08
Komal Rathi <komalrathi@Komals-MacBook-Pro.local>
2025-01-06 21:06:34 +0000

    add expect tests for while loop (parsing and typing stages)

---
+++ test/expect/parsing/test_while_loop_success.ml
@@ -0,0 +1,19 @@
+open Core
+open Print_parsed_ast
+
+let%expect_test "Successful While loop parsing" =
+  print_parsed_ast (Lexing.from_string "while (x < 5) { x:= x + 1 }") ;
+  [%expect {|
+    Program([
+
+    ], While(CompOp(LessThan, Identifier(x), Integer(5)), Assign(x, BinOp(Plus, Identifier(x), Integer(1)))))
+    |}]
+
+let%expect_test "Successful While loop parsing with let statement" =
+  print_parsed_ast
+    (Lexing.from_string "let x:(int, Low) = 5 in while (x < 5) { x:= x + 1 }") ;
+  [%expect {|
+    Program([
+
+    ], Let(x, (Int, Low), Integer(5), While(CompOp(LessThan, Identifier(x), Integer(5)), Assign(x, BinOp(Plus, Identifier(x), Integer(1))))))
+    |}]
