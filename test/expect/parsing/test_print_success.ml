6400d3b9567406daf0fd074d65f5b0b6a1e825d1
Komal Rathi <komalrathi@dhcp-10-249-113-57.eduroam.wireless.private.cam.ac.uk>
2025-01-20 21:02:38 +0000

    add example programs + more expect tests for print

--- test/expect/parsing/test_print_success.ml
+++ test/expect/parsing/test_print_success.ml
@@ -11,10 +11,39 @@ let%expect_test "Normal Print Statement" =
     |}]
 
 let%expect_test "Secure Print Statement" =
-  print_parsed_ast (Lexing.from_string "let x:(int,High) = 5 in securePrint(x)") ;
+  print_parsed_ast
+    (Lexing.from_string "let x:(int,High) = 5 in securePrint(x)") ;
   [%expect
     {|
     Program([
 
     ], Let(x, (Int, High), Integer(5), SecurePrint([Identifier(x)])))
+    |}]
+
+let%expect_test "Print with while" =
+  print_parsed_ast
+    (Lexing.from_string
+       "let test_var:(int,Low) = 3 in \n\
+       \        while (test_var < 6) {\n\
+       \            test_var := test_var + 1\n\
+       \        };\n\
+       \               print(test_var)\n\
+       \    " ) ;
+  [%expect
+    {|
+    Program([
+
+    ], Let(test_var, (Int, Low), Integer(3), Seq(While(CompOp(LessThan, Identifier(test_var), Integer(6)), Assign(test_var, BinOp(Plus, Identifier(test_var), Integer(1)))), Print([Identifier(test_var)]))))
+    |}]
+
+let%expect_test "Print with while" =
+  print_parsed_ast
+    (Lexing.from_string
+       "let x:(int,Low) = 6 in (let y:(int,Low) = 0 in while (y < 5) { \
+        print(x); y := y+1 })" ) ;
+  [%expect
+    {|
+    Program([
+
+    ], Let(x, (Int, Low), Integer(6), Let(y, (Int, Low), Integer(0), While(CompOp(LessThan, Identifier(y), Integer(5)), Seq(Print([Identifier(x)]), Assign(y, BinOp(Plus, Identifier(y), Integer(1))))))))
     |}]
