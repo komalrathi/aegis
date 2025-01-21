c5c6c54ab231b82c0acc50c13c09338c1c85e206
Komal Rathi <komalrathi@dhcp-10-249-113-57.eduroam.wireless.private.cam.ac.uk>
2025-01-21 00:27:16 +0000

    wip - tests for fibonacci i.e. checking recursion

---
+++ test/expect/typing/test_fib.ml
@@ -0,0 +1,27 @@
+open Core
+open Print_typed_ast
+
+let%expect_test "fib" =
+  match
+    Parser_frontend.Parse_program.parse_program
+      (Lexing.from_string
+         "fn fib(x:(int,Low)):(int, Low) {\n\
+         \    if ((x == 1)|| (x == 0)) then {\n\
+         \        1\n\
+         \    }\n\
+         \    else {\n\
+         \        fib(x - 1) + fib(x - 2)\n\
+         \    }\n\
+          };\n\
+          fib(3)" )
+  with
+  | Ok program -> print_typed_ast program
+  | Error _ ->
+      print_endline "Error: could not parse program" ;
+      [%expect.unreachable] ;
+      [%expect
+        {|
+    Program([
+    FunctionDefn(fib, [x: (Int, Low)], (Int, Low), If(BoolOp(Or, (Bool, Low), CompOp(Equality, (Bool, Low), Identifier(x, (Int, Low)), Integer(1)), CompOp(Equality, (Bool, Low), Identifier(x, (Int, Low)), Integer(0))), Integer(1), BinOp(Plus, (Int, Low), FunctionApp(fib, (Int, Low), [BinOp(Minus, (Int, Low), Identifier(x, (Int, Low)), Integer(1))]), FunctionApp(fib, (Int, Low), [BinOp(Minus, (Int, Low), Identifier(x, (Int, Low)), Integer(2))])), (Int, Low)))
+    ], FunctionApp(fib, (Int, Low), [Integer(3)]))
+    |}]
