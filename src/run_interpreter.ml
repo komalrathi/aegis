a41465ca4b452a994a35cefad3f9650880bcb064
Komal Rathi <komalrathi@dhcp-10-249-121-30.eduroam.wireless.private.cam.ac.uk>
2025-01-27 20:37:19 +0000

    add VObject support to string_of_val in run_interpreter

--- src/run_interpreter.ml
+++ src/run_interpreter.ml
@@ -4,19 +4,22 @@ open Typing.Type_program
 open Interpreter.Interpret_program
 open Compiler_types.Language_types
 
-let string_of_val = function
+let rec string_of_val = function
   | VInt i -> string_of_int i
   | VBool b -> string_of_bool b
   | VUnit _ -> "unit"
+  | VObject (obj, args) ->
+      let args_str =
+        String.concat ~sep:"; "
+          (Stdlib.List.map
+             (fun value -> Printf.sprintf "%s" (string_of_val value))
+             args )
+      in
+      Printf.sprintf "VObject(%s, {%s})" obj args_str
 
 let print_result = function
   | Ok v -> printf "Success: %s" (string_of_val v)
   | Error e -> eprintf "%s" (Error.to_string_hum e)
-
-(* let run_interpreter lexbuf = let ( >>= ) = Result.( >>= ) in parse_program
-   lexbuf >>= type_program >>= interpret_program |> function | Ok v -> printf
-   "Success: %s" (string_of_val v) | Error e -> eprintf "%s"
-   (Error.to_string_hum e) *)
 
 let run_interpreter lexbuf =
   let ( >>= ) = Result.( >>= ) in
