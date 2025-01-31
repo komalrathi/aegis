b007da320625543bff72fbd35f37a54e381acef6
Komal Rathi <komalrathi@Komals-MacBook-Pro.local>
2025-01-31 13:26:48 +0000

    add expect tests for parsing and typing stages of class - it works

--- test/expect/parsing/test_class_success.ml
+++ test/expect/parsing/test_class_success.ml
@@ -8,16 +8,16 @@ let%expect_test "Example Class" =
        \    test_var:(int, High);\n\
        \    test_bool:(bool,Low)\n\n\
        \    constructor (x:(int,Low), y:(int, Low)) {\n\
-       \        test_bool:= True;\n\
+       \        test_bool := True;\n\
        \        test_var := x+y\n\
-       \    };\n\
-       \    Low fn sum(z:(int, Low)) : (int, Low) {\n\
+       \    }\n\
+       \    High fn sum(z:(int, Low)) : (int, High) {\n\
        \        if (test_bool) then{\n\
-       \            x := z+6\n\
+       \            test_var := z+6\n\
        \        }\n\
        \        else\n\
        \        {\n\
-       \            x := test_var +6\n\
+       \            test_var := test_var + 6\n\
        \        }\n\
        \    };\n\
         };\n\
@@ -26,7 +26,7 @@ let%expect_test "Example Class" =
     {|
     Program([
     ClassDefn(Example, FieldDefn(test_var, (Int, High))
-    FieldDefn(test_bool, (Bool, Low)), Constructor([x: (Int, Low); y: (Int, Low)], Assign(test_bool, Seq(Boolean(true), Assign(test_var, BinOp(Plus, Identifier(x), Identifier(y)))))), MethodDefn(Low, FunctionDefn(sum, [z: (Int, Low)], (Int, Low), If(Identifier(test_bool), Assign(x, BinOp(Plus, Identifier(z), Integer(6))), Assign(x, BinOp(Plus, Identifier(test_var), Integer(6)))))))
+    FieldDefn(test_bool, (Bool, Low)), Constructor([x: (Int, Low); y: (Int, Low)], Seq(Assign(test_bool, Boolean(true)), Assign(test_var, BinOp(Plus, Identifier(x), Identifier(y))))), MethodDefn(High, FunctionDefn(sum, [z: (Int, Low)], (Int, High), If(Identifier(test_bool), Assign(test_var, BinOp(Plus, Identifier(z), Integer(6))), Assign(test_var, BinOp(Plus, Identifier(test_var), Integer(6)))))))
     ],[
 
     ], Let(y, (Int, Low), Integer(72), BinOp(Minus, Identifier(y), Integer(5))))
--- test/expect/typing/test_class_success.ml
+++ test/expect/typing/test_class_success.ml
@@ -11,14 +11,14 @@ let%expect_test "Example Class" =
          \    constructor (x:(int,Low), y:(int, Low)) {\n\
          \        test_bool := True;\n\
          \        test_var := x+y\n\
-         \    };\n\
-         \    Low fn sum(z:(int, Low)) : (int, Low) {\n\
+         \    }\n\
+         \    High fn sum(z:(int, Low)) : (int, High) {\n\
          \        if (test_bool) then{\n\
-         \            x := z+6\n\
+         \            test_var := z+6\n\
          \        }\n\
          \        else\n\
          \        {\n\
-         \            x := test_var +6\n\
+         \            test_var := test_var + 6\n\
          \        }\n\
          \    };\n\
           };\n\
@@ -32,5 +32,16 @@ let%expect_test "Example Class" =
       [%expect.unreachable] ;
       [%expect.unreachable] ;
       [%expect.unreachable] ;
+      [%expect.unreachable] ;
+      [%expect.unreachable] ;
+      [%expect.unreachable] ;
+      [%expect.unreachable] ;
       [%expect.unreachable];
-  [%expect {| Variable test_bool core type (Bool) does not match the assigned core type (Int) |}]
+  [%expect {|
+    Program([
+    ClassDefn(Example, FieldDefn(test_var, (Int, High))
+    FieldDefn(test_bool, (Bool, Low)), Constructor([x: (Int, Low); y: (Int, Low)], Seq(Assign((Bool, Low), test_bool, Boolean(true)), Assign((Int, High), test_var, BinOp(Plus, (Int, Low), Identifier(x, (Int, Low)), Identifier(y, (Int, Low)))), (Int, High))), MethodDefn(High, FunctionDefn(sum, [z: (Int, Low)], (Int, High), If(Identifier(test_bool, (Bool, Low)), Assign((Int, High), test_var, BinOp(Plus, (Int, Low), Identifier(z, (Int, Low)), Integer(6))), Assign((Int, High), test_var, BinOp(Plus, (Int, High), Identifier(test_var, (Int, High)), Integer(6))), (Int, High)))))
+    ],[
+
+    ], Let(y, (Int, Low), Integer(72), BinOp(Minus, (Int, Low), Identifier(y, (Int, Low)), Integer(5)), (Int, Low)))
+    |}]
