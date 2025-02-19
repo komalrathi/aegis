c6e2e1e35c0124e178364fac65d3179eda0d9a8a
Komal Rathi <komalrathi@Komals-MacBook-Pro.local>
2025-02-19 16:15:37 +0000

    update interpret_program to have interpret_expr_result Or_error.t

--- src/interpreter/interpret_program.ml
+++ src/interpreter/interpret_program.ml
@@ -8,5 +8,9 @@ let interpret_program (Prog (typed_class_defns, typed_fn_defns, typed_expr))
   let ( >>= ) = Result.( >>= ) in
   interpret_fn_defns typed_fn_defns []
   >>= fun function_environment ->
-  interpret_expr typed_expr [] function_environment typed_class_defns
-  >>= fun (output_val, _) -> Ok output_val
+  match
+    interpret_expr typed_expr [] function_environment typed_class_defns
+  with
+  | Ok (IValue (output_val, _)) -> Ok output_val
+  | Ok (IException (_, _)) -> Error (Error.of_string "Exception Raised")
+  | Error err -> Error err
